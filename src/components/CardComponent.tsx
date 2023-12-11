6763e68ec009651f49d1b6dcd2fa66ef07ed7438
Piriya <piriyalapa@gmail.com>
2023-12-11 23:24:58 +0700

    install dependencies

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -11,6 +11,8 @@ export const CardContainer = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `;
 
+
+
 export class Card extends Component {
   render():ReactNode {
     return (
