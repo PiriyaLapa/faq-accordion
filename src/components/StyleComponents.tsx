09240d217bd6043f1aae3d12d393db529efaa8bd
Piriya <piriyalapa@gmail.com>
2023-12-15 18:17:38 +0700

    test to deploy 3

--- src/components/StyleComponents.tsx
+++ src/components/StyleComponents.tsx
@@ -1,35 +1,33 @@
-// import styled, { createGlobalStyle } from "styled-components";
-// import { ReactNode } from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+import { ReactNode } from "react";
 
-// const GlobalStyles = createGlobalStyle`
-//   @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@0;1&family=Poppins&family=Roboto:wght@500&family=Ubuntu&family=Work+Sans:wght@700&display=swap');
-// `;
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@0;1&family=Poppins&family=Roboto:wght@500&family=Ubuntu&family=Work+Sans:wght@700&display=swap');
+`;
 
-// interface StyleComponentProps {
-//   children: ReactNode;
-// }
+interface StyleComponentProps {
+  children: ReactNode;
+}
 
-// export const WorkSans = createStyleComponent("div", "Work Sans", 700);
-// function createStyleComponent(
-//   elementType: string,
-//   fontFamily: string,
-//   fontWeight: number
-// ) {
-//   return styled(elementType)<StyleComponentProps>`
-//     font-family: ${fontFamily}, sans-serif;
-//     font-weight: ${fontWeight || "normal"};
-//     display: inline-block;
-//     padding: 5px;
-//     margin: 5px 0px;
-//   `;
-// }
+export const WorkSans = createStyleComponent("div", "Work Sans", 700);
+function createStyleComponent(
+  elementType: string,
+  fontFamily: string,
+  fontWeight: number
+) {
+  return styled(elementType)<StyleComponentProps>`
+    font-family: ${fontFamily}, sans-serif;
+    font-weight: ${fontWeight || "normal"};
+    display: inline-block;
+    padding: 5px;
+    margin: 5px 0px;
+  `;
+}
 
 export const Container = styled.div`
   width: 100%;
   height: 900px;
   margin: 0 auto;
-
 `;
 export const Header = styled(Container)`
   height: 320px;
